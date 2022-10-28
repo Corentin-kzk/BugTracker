408245cd7c179a8645f10fb1114c248ab31f1514
corentin-koka <corentin@groupesanctuaire.com>
2022-10-28 13:45:12 +0200

    add s

--- env.js
+++ env.js
@@ -1,5 +1,5 @@
 const URL = "http://127.0.0.1:5500";
-const APIURL = "http://greenvelvet.alwaysdata.net/bugTracker/api";
+const APIURL = "https://greenvelvet.alwaysdata.net/bugTracker/api";
 const USRTK = 'dt-usr'
 
 export { URL, APIURL, USRTK };
